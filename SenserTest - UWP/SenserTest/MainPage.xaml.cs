d06cf93b8dde8e090b98c9b89fe7de98513ed936
Sepi <deet1022@gmail.com>
2016-08-12 21:51:19 +0900

    First commit!

---
+++ SenserTest - UWP/SenserTest/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SenserTest
+{
+    public sealed partial class MainPage : Page
+    {
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.Unloaded += MainPage_Unloaded;
+
+            var test = Senser.Distance.GetDistance();
+            textBlock.Text = $"{test}cm";
+
+            timer.Interval = TimeSpan.FromSeconds(0.5);
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            var distance = Senser.Distance.GetDistance();
+            textBlock.Text = $"{distance}cm";
+        }
+
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (timer.IsEnabled)
+            {
+                timer.Tick -= Timer_Tick;
+                timer.Stop();
+            }
+        }
+    }
+}
---
+++ SenserTest - UWP/SenserTest/Senser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Windows.Devices.Gpio;
+
+namespace SenserTest
+{
+    public class Senser
+    {
+        private GpioController gpio;
+
+        private GpioPin triggerPin;
+        private GpioPin echoPin;
+
+        private int PIN_ECHO = 18; // Gpio 18
+        private int PIN_TRIG = 23; // Gpio 23
+
+        public static Senser Distance { get; } = new Senser();
+
+        private Senser()
+        {
+            gpio = GpioController.GetDefault();
+            if (gpio == null)
+            {
+                return;
+            }
+
+            this.triggerPin = gpio.OpenPin(PIN_TRIG);
+            this.echoPin = gpio.OpenPin(PIN_ECHO);
+
+            this.triggerPin.SetDriveMode(GpioPinDriveMode.Output);
+            this.echoPin.SetDriveMode(GpioPinDriveMode.Input);
+
+            this.triggerPin.Write(GpioPinValue.Low);
+        }
+
+        public double GetDistance()
+        {
+            ManualResetEvent mre = new ManualResetEvent(false);
+            Stopwatch stopwatch = new Stopwatch();
+
+            this.triggerPin.Write(GpioPinValue.High);
+            mre.WaitOne(TimeSpan.FromMilliseconds(0.01));
+            this.triggerPin.Write(GpioPinValue.Low);
+
+            while (this.echoPin.Read() == GpioPinValue.Low)
+            {
+            }
+            stopwatch.Start();
+
+            while (this.echoPin.Read() == GpioPinValue.High)
+            {
+            }
+            stopwatch.Stop();
+
+            TimeSpan timeBetween = stopwatch.Elapsed;
+            Debug.WriteLine(timeBetween.ToString());
+            double distance = timeBetween.TotalSeconds * 17000;
+
+            return Math.Round(distance);
+        }
+    }
+}
